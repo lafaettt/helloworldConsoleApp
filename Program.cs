11fcfc82d6c2a6bdf5861cfbe7364c69f9637515
mario <mario@DESKTOP-6OES14S>
2023-09-18 19:48:36 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworldConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello Wold!!!");
+            Console.Write("Hello ");
+            Console.Write("World ");
+            Console.Write("!!!");
+
+            Console.ReadKey();
+        }
+    }
+}
